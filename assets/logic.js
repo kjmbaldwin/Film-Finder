eeb84ca5fcf21f47eb5340e68cdaa6367425e540
Kyle Baldwin <kylejmbaldwin@gmail.com>
2017-12-04 22:47:23 -0500

    created logic for OMDB api call

--- assets/logic.js
+++ assets/logic.js
@@ -9,3 +9,40 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+
+
+//api keys
+var OmdbKey = '15c27a54';
+
+// $('#search-input').onkeypress(function(){ 
+
+// });
+
+
+$('#search-btn').on('click', function(){
+
+  var OmdbSearch = $('#search-input').val().trim();
+
+  $.ajax({
+  url: 'https://www.omdbapi.com/?s=' + OmdbSearch + '&plot=short&apikey=' + OmdbKey,
+  method: "GET"
+  }).done(function(response) {
+
+    var movieArr = response.Search;
+    console.log(movieArr);
+    
+    for (var i = 0; i < movieArr.length; i++) {
+      var $results = $('<div>').addClass("panel panel-default").text(movieArr[i].Title);
+      $('#results').append($results);
+    
+    }
+  
+  });
+});
+
+
+
+
+
+
+ // on click search-btn
---
+++ keys.md
@@ -0,0 +1,8 @@
+Google search key:
+AIzaSyAGSPBSjed8daG1-7MTPi7H4HYHchsbeQQ
+
+OMDB:
+key: 15c27a54
+url: http://www.omdbapi.com/?i=tt3896198&apikey=15c27a54
+docs: http://omdbapi.com/
+
