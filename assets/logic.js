2122f323628cf58e743594558bbf9a62931ae534
Annie Flaherty <anne.flaherty@case.edu>
2017-12-11 19:09:16 -0500

    Fixed data attributes on Add to Favorites button

--- assets/logic.js
+++ assets/logic.js
@@ -1,17 +1,16 @@
-<<<<<<< HEAD
 // beginning
-var config = {
-  apiKey: "AIzaSyBlAgFLX4MoD4fxQ8b_O-FCJ4SvNyFGPtg",
-  authDomain: "api-group-project.firebaseapp.com",
-  databaseURL: "https://api-group-project.firebaseio.com",
-  projectId: "api-group-project",
-  storageBucket: "api-group-project.appspot.com",
-  messagingSenderId: "1016861619283"
-};
-firebase.initializeApp(config);
+// var config = {
+//   apiKey: "AIzaSyBlAgFLX4MoD4fxQ8b_O-FCJ4SvNyFGPtg",
+//   authDomain: "api-group-project.firebaseapp.com",
+//   databaseURL: "https://api-group-project.firebaseio.com",
+//   projectId: "api-group-project",
+//   storageBucket: "api-group-project.appspot.com",
+//   messagingSenderId: "1016861619283"
+// };
+// firebase.initializeApp(config);
 
 
-var database = firebase.database();
+// var database = firebase.database();
 
 
 //##### OMDB API Calls #########
@@ -121,6 +120,7 @@ function movieInfoDisplay(imdbID) {
         // favorite button
         var favBtn = $('<button>').text(' Add to favorites');
         favBtn.addClass('add btn btn-default');
+        favBtn.attr('data-movie-id', imdbID);
 
         var iconSpan = $('<span>');
         iconSpan.addClass('glyphicon glyphicon-star');
